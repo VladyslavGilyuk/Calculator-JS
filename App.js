4b56a5bae444564d9cfb0058b617ffd30d8dfced
Vladyslav Gilyuk <107754352+VladyslavGilyuk@users.noreply.github.com>
2022-09-11 01:11:59 +0300

    Comment aded

--- App.js
+++ App.js
@@ -1,33 +1,34 @@
 const display = document.getElementById("display")
 let buttons = Array.from(document.getElementsByClassName('button'));
- 
+
+/*performs corresponding action on clicked btn */
 buttons.map (button => {
-    button.addEventListener("click", (e) => {
-        switch(e.target.innerText) {
-            case "AC" :
-                display.innerText = "";
-                break;
-            case "←" :
-                if (display.innerText) {
-                display.innerText = display.innerText.slice(0, -1)
-             } 
-             break;
-             case "=" :
-                try {
-                   if (display.innerText == "") {
-                    display.innerText = "Empty!"
-                    setTimeout (() => (display.innerText = ""), 1000)
-                  } else {
-                      display.innerText = eval(display.innerText);
-                    }
-                }
-                catch {
-                    display.innerText = "Error"
-                    setTimeout (() => (display.innerText = ""), 1000)
-                }
-            break;
-        default :
-            display.innerText +=e.target.innerText
-            }      
-    });
+  button.addEventListener("click", (e) => {
+    switch(e.target.innerText) {
+      case "AC" :
+        display.innerText = "";
+      break;
+      case "←" :
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1)
+        } 
+       break;
+       case "=" :
+        try {
+           if (display.innerText == "") {
+              display.innerText = "Empty!"
+              setTimeout (() => (display.innerText = ""), 1000)
+           } else {
+            display.innerText = eval(display.innerText);
+           }
+        }
+        catch {
+          display.innerText = "Error"
+          setTimeout (() => (display.innerText = ""), 1000)
+        }
+      break;
+     default :
+       display.innerText +=e.target.innerText
+    }    
+  });
 })
